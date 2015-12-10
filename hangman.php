5a60bc998d16c68363625c478df32e3e9e1f2000
Bertrand Moulard <bmoulard@etsy.com>
2015-12-10 15:32:18 -0500

    validates input and ignores duplicate tries

--- hangman.php
+++ hangman.php
@@ -6,11 +6,9 @@ class Hangman {
   private $word;
   private $good_letters = [];
   private $bad_letters = [];
-  private $drawer;
 
   public function __construct() {
     echo "Starting the game\n";
-    $this->drawer = new HangmanDrawer();
     $this->word = str_split(
       strtolower(
         file_get_contents("http://randomword.setgetgo.com/get.php?len=10")
@@ -25,7 +23,6 @@ class Hangman {
     echo "Remaining wrong guesses: " . (self::LIMIT - count($this->bad_letters)) . "\n";
     echo "Bad guesses: [" . join(", ", $this->bad_letters) . "]\n";
     $this->printWordInProgress();
-    $this->drawer->draw(count($this->bad_letters);
     echo "\n\n\n\n";
   }
 
@@ -53,16 +50,35 @@ class Hangman {
     $this->listenForInput();
   }
 
+  private function validateInput($input) {
+    if(strlen($input) != 1) {
+      return false;
+    }
+    if(!ctype_alpha($input)) {
+      return false;
+    }
+    return true;
+  }
+
   private function handleInput($input) {
-    if(in_array($input, $this->word)) {
-      array_push($this->good_letters, $input);
-    } else {
-      array_push($this->bad_letters, $input);
-      if(count($this->bad_letters) >= self::LIMIT) {
-        echo "The word is " . join("", $this->word) . "\n";
-        echo "Game over!\n";
-        exit(0);
+    $input = strtolower($input);
+    if($this->validateInput($input)) {
+      if(in_array($input, $this->bad_letters) || in_array($input, $this->good_letters)) {
+        echo "You previously entered this letter, pick again!\n";
+      } else {
+        if(in_array($input, $this->word)) {
+          array_push($this->good_letters, $input);
+        } else {
+          array_push($this->bad_letters, $input);
+          if(count($this->bad_letters) >= self::LIMIT) {
+            echo "The word is " . join("", $this->word) . "\n";
+            echo "Game over!\n";
+            exit(0);
+          }
+        }
       }
+    } else {
+      echo "ERROR: Please enter a single letter\n\n";
     }
     $this->printState();
   }
